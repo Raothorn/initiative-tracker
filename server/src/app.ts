f18f4f1f60e219a8bd6f0505957a1dc4d10da942
Gaston Gilbert <gastongator@gmail.com>
2024-08-20 12:46:31 -0500

    Created 'share' project and got it imported

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Monster } from 'share';
 
 const app = express();
 const server = createServer(app);
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    let m: Monster = { name: "goblin" }
+    console.log(m)
+    console.log('server running at http://localhost:3000')
 });
 
